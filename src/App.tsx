61a0d7fe8753bc09740c3c281513a3544b7b5954
Eric Phung <esphung@gmail.com>
2025-08-16 20:00:48 -0500

    test: add gh actions
    test: change actions name

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,39 @@
-/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet, Button } from 'react-native';
 
-const App = () => {
+function App() {
+  const [count, setCount] = React.useState<number>(0);
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#f0f0f0',
-      }}
-    >
-      <Text style={{ fontSize: 20 }}>Hello, World!</Text>
+    <View style={styles.container}>
+      <View style={styles.northPanel}>
+        <Text style={styles.text}>Count: {count}</Text>
+      </View>
+      <View style={styles.southPanel}>
+        <Button title="Increment" onPress={() => setCount(count + 1)} />
+      </View>
     </View>
   );
-};
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  text: {
+    fontSize: 20,
+  },
+  northPanel: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#e0e0e0',
+  },
+  southPanel: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#d0d0d0',
+  },
+});
 
 export default App;
